2b360a42c8b33be555c94fd1aeb7faa2958adf4d
Documenter.jl <documenter@juliadocs.github.io>
2021-10-28 21:01:08 +0000

    build based on d7989c4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Bosonic","category":"page"},{"location":"#Bosonic","page":"Home","title":"Bosonic","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Bosonic.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Bosonic]","category":"page"}]
+}
